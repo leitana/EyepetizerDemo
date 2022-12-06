04bc534aa33cd7e449600f0302a810aed507ee06
linxiao <1130037266@qq.com>
2022-12-06 16:37:28 +0800

    BaseViewModel

---
+++ lib_common/src/main/java/com/lx/common/mvvm/viewmodel/BaseViewModel.kt
@@ -0,0 +1,10 @@
+package com.lx.common.mvvm.viewmodel
+
+/**
+ * @title：BaseViewModel
+ * @projectName EyepetizerDemo
+ * @description: <Description>
+ * @author linxiao
+ * @data Created in 2022/12/06
+ */class BaseViewModel {
+}
